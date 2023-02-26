6c559ec993567ea3ed88d37db7e0701deddbe7a7
Carlosayol <Carlosaolar@gmail.com>
2023-02-26 12:07:51 -0500

    feat: update about page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,10 @@
+import NextLink from "next/link";
 import { Inter } from "@next/font/google";
-import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import { Box, Button, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 import Section from "@/components/section";
+import Paragraph from "@/components/paragraph";
+import { AboutSection } from "@/components/about";
+import { DownloadIcon } from "@chakra-ui/icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +25,7 @@ const Home = () => {
           <Heading as="h2" variant="page title">
             Carlos Ayala
           </Heading>
-          <p>Developer</p>
+          <p>Backend Developer</p>
         </Box>
         <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
           <Image
@@ -38,9 +42,27 @@ const Home = () => {
       </Box>
       <Section>
         <Heading as="h3" variant="section-title">
-          Work
+          About Me
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          Software Engineer with 2+ years of experience as a Backend Developer designing, developing and maintaining
+          Shopify applications, web applications, databases, APIs and other backend solutions with NodeJS, Python and Go
+          with technologies and frameworks such as Express, NestJS, Django and AWS, with an interest in increasing his
+          knowledge and skills in developing innovative solutions using new technologies and the best practices.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <Button as={NextLink} href="/works" scroll={false} rightIcon={<DownloadIcon />} colorScheme="teal">
+            Download CV
+          </Button>
+        </Box>
+      </Section>
+      <Section>
+        <Heading as="h3" variant="section-title">
+          Likes
+        </Heading>
+        <Paragraph>
+          Music, Playing Violin & Piano, Biking, Reading, Video Games, Coding, Artificial Intelligence, Good Vibes
+        </Paragraph>
       </Section>
     </Container>
   );
