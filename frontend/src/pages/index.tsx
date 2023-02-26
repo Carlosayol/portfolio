ebc25531b0468a879fbe6349282afd6076980cd5
Carlosayol <Carlosaolar@gmail.com>
2023-02-26 10:44:14 -0500

    feat: add image icon

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { Inter } from "@next/font/google";
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="teal" p={3} mb={6} alignContent="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        p={3}
+        mb={6}
+        alignContent="center"
+      >
         Hi, I&apos;m a full-stack developer
       </Box>
       <Box display={{ md: "flex" }}>
@@ -15,6 +21,18 @@ const Home = () => {
             Carlos Ayala
           </Heading>
           <p>Developer</p>
+        </Box>
+        <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="images/carlos.jpg"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
